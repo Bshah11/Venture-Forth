67a13757c6f07fd398aee0b70976d350483fcc27
KC Willard <willarke@oregonstate.edu>
2021-04-26 17:39:20 -0700

    tried moving the event listeners into variables to make them easy to remove

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -168,50 +168,60 @@ var lastLine;
 //Get default values from the form selection
 var formLineColor = document.getElementById("colorDropdownMenu");
 var formStrokeWidth = document.getElementById("widthDropdownMenu");
+
+//drawLine event listners
+var drawLineMouseDown = function(e){
+    console.log(isDrawing)
+    isDrawing = true;
+    console.log("isDrawing after call:"+isDrawing);
+    var pos = stage.getPointerPosition();
+    lastLine = new Konva.Line({
+        stroke: color,
+        strokeWidth: width,
+        points: [((Math.round(pos.x/cellSize)) * cellSize), ((Math.round(pos.y/cellSize)) * cellSize)],
+        category: 'line',
+    });
+    mapLayer.add(lastLine);
+};
+
+var drawLineMouseUp = function(){
+    const pos = stage.getPointerPosition();
+    console.log(isDrawing)
+    var newPoints = lastLine.points().concat([(Math.round(pos.x/cellSize)) * cellSize, (Math.round(pos.y/cellSize)) * cellSize]);
+    lastLine.points(newPoints);
+    isDrawing = false;
+    mapLayer.batchDraw()
+    saveMapLayer(mapLayer);
+};
+
+var drawLineMouseMove = function(){
+    if (!isDrawing) {
+        return;
+    };
+    const pos = stage.getPointerPosition();
+    //make sure line ends on grid intersection
+    if (lastLine.points().length = 2){
+        var newPoints = lastLine.points().concat([(Math.round(pos.x/cellSize)) * cellSize, (Math.round(pos.y/cellSize)) * cellSize]);
+    } else {
+        newPoints = lastLine.points()[2] = (Math.round(pos.x/cellSize)) * cellSize;
+        newPoints = lastLine.points()[3] = (Math.round(pos.y/cellSize)) * cellSize;
+    }
+    lastLine.points(newPoints);
+    mapLayer.batchDraw()
+    saveMapLayer(mapLayer);
+};
 //Add line drawing to map layer
 function drawLine(color, width){
+    //Remove all event listeners from stage before adding new ones
+    //stage.removeEventListener('mousedown touchstart')
     console.log("in drawLine");
     // All functions reference the stage but write to the map layer
-    stage.on('mousedown touchstart', (e) =>{
-        console.log(isDrawing)
-        isDrawing = true;
-        var pos = stage.getPointerPosition();
-        lastLine = new Konva.Line({
-            stroke: color,
-            strokeWidth: width,
-            points: [((Math.round(pos.x/cellSize)) * cellSize), ((Math.round(pos.y/cellSize)) * cellSize)],
-            category: 'line',
-        });
-        mapLayer.add(lastLine);
-    });
-
-    stage.on('mouseup touchend', () =>{
-        const pos = stage.getPointerPosition();
-        console.log(isDrawing)
-        var newPoints = lastLine.points().concat([(Math.round(pos.x/cellSize)) * cellSize, (Math.round(pos.y/cellSize)) * cellSize]);
-        lastLine.points(newPoints);
-        isDrawing = false;
-        mapLayer.batchDraw()
-        saveMapLayer(mapLayer);
-    });
+    stage.on('mousedown touchstart', drawLineMouseDown);
+    stage.on('mouseup touchend', drawLineMouseUp);
+    stage.on('mousemove touchmove', drawLineMouseMove);
 
     //This is to draw and snap to grid
-    stage.on('mousemove touchmove', () =>{
-        if (!isDrawing) {
-            return;
-        };
-        const pos = stage.getPointerPosition();
-        //make sure line ends on grid intersection
-        if (lastLine.points().length = 2){
-            var newPoints = lastLine.points().concat([(Math.round(pos.x/cellSize)) * cellSize, (Math.round(pos.y/cellSize)) * cellSize]);
-        } else {
-            newPoints = lastLine.points()[2] = (Math.round(pos.x/cellSize)) * cellSize;
-            newPoints = lastLine.points()[3] = (Math.round(pos.y/cellSize)) * cellSize;
-        }
-        lastLine.points(newPoints);
-        mapLayer.batchDraw()
-        saveMapLayer(mapLayer);
-    });
+
 
 }
 var drawLineButton = document.getElementById('draw-line-button');
